3cb5bdd4988add25b6cc17b4fee2b1486e4ce4ca
cortelucas <corte1994.lc@gmail.com>
2023-03-30 14:50:07 -0300

    entendendo o docker compose

--- docker/node/index.js
+++ docker/node/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const api = express()
 const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Lucas')`
+
+connection.query(sql)
+connection.end()
 
 api.get('/', (request, response) => {
   response.send('<h1>Full Cycle</h1>')
