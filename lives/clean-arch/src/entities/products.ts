f9e8331c133a6fc074cf0f51fb7b7b93b46b4777
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2023-06-26 02:08:55 -0300

    feat: implement product entity

---
+++ lives/clean-arch/src/entities/products.ts
@@ -0,0 +1,38 @@
+export default class Product {
+  _name: string
+  _cost: number
+  _salesPrice: number
+  _id: string
+
+  constructor (id: string, name?: string) {
+    this._name = name
+    this._id = id
+  }
+
+  get id (): string {
+    return this._id
+  }
+
+  get name (): string {
+    return this._name
+  }
+
+  set name (name: string) {
+    this._name = name
+  }
+
+  set cost (cost: number) {
+    if (cost < 0) {
+      throw new Error('Cost cannot be negative.')
+    }
+    this._cost = cost
+  }
+
+  get cost (): number {
+    return this._cost
+  }
+
+  get salesPrice (): number {
+    return this._cost * 3
+  }
+}
